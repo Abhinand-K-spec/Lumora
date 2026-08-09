de364a05e3d4453d3926e2c2e69459e2b183c37d
Abhinand k <abhinnanndk@gmail.com>
2026-08-09 23:40:33 +0530

    feat(redis) : redis config added

---
+++ backend/src/shared/config/redis.ts
@@ -0,0 +1,17 @@
+import { createClient } from "redis";
+
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL!,
+});
+
+redisClient.on("error", (error) => {
+    console.error("Redis error:", error);
+});
+
+export const connectRedis = async () => {
+    await redisClient.connect();
+    console.log("Redis connected");
+};
+
+export default redisClient;
