bc441e5e69a630411a254d7af74655a52403d8d6
Abhinand k <abhinnanndk@gmail.com>
2026-08-09 23:52:32 +0530

    feat+fix(auth) : i otp repository implemented and the enum for otp purpose had an error now cleared it

---
+++ backend/src/modules/auth/interfaces/IOTPRepository.ts
@@ -0,0 +1,15 @@
+import { otpPurpose } from "../../../shared/enums/OTPPurpose"
+
+export interface IOTPRepository{
+    save(
+        userId:string,
+        purpose:otpPurpose,
+        otp:string,
+        expiresIn:number
+
+    ):Promise<void>;
+    
+    get(userId:string,purpose:otpPurpose):Promise<string| null>;
+
+    delete(userOd:string,purpose:otpPurpose):Promise<void>;
+}
