8d1beccd55d6ab2064e36d937a0372ee42c011a2
Abhinand k <abhinnanndk@gmail.com>
2026-08-21 12:25:02 +0530

    feat(user profile) : multer,cloudinary installed and cloudinary configured

---
+++ backend/src/shared/config/cloudinary.ts
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import { AppError } from "../errors/AppError";
+import { HttpStatus } from "../enums/HTTP.status.code";
+
+const name = process.env.CLOUDINARY_CLOUD_NAME;
+const key = process.env.CLOUDINARY_API_KEY;
+const secret = process.env.CLOUDINARY_API_SECRET;
+
+if (!name || !key || !secret) {
+  throw new AppError(HttpStatus.BAD_REQUEST, "Cloudinary keys are missing");
+}
+cloudinary.config({
+  cloud_name: name,
+  api_key: key,
+  api_secret: secret,
+});
+
+export const uploadToCloudinary = (
+  fileBuffer: Buffer,
+  folder: string
+): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { folder: folder, resource_type: "auto" },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result!.secure_url);
+      }
+    );
+    uploadStream.end(fileBuffer);
+  });
+};
+
+export default cloudinary;
